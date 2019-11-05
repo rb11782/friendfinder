2b972299e5d1d7bdf50a8d6d4afd5d976b5fd73a
Raymond Bertrand <rb11782@gmail.com>
2019-11-05 22:45:11 +0000

    I have added a top navigation.

--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe FriendsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "friends#new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
 end
