3e2fa67b6eb8fce285e7d1fc38a890cc01c7f0f5
Raymond Bertrand <rb11782@gmail.com>
2019-11-07 17:29:50 +0000

    I have added factorybot to my application and I have written more rspec tests.

--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -9,7 +9,17 @@ RSpec.describe FriendsController, type: :controller do
   end
 
   describe "friends#new action" do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
+
+
     it "should successfully show the new form" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
       get :new
       expect(response).to have_http_status(:success)
     end
@@ -17,13 +27,45 @@ RSpec.describe FriendsController, type: :controller do
 
 
   describe "friends#create action" do
+    
+    it "should require users to be logged in" do
+      post :create, params: { friend: { name: 'John', address: '21 Saturn Court, Sudbury, Ontario P3E 6B8' } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+
     it "should successfully create a new friend in our database" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
       post :create, params: { friend: { name: 'John', address: '21 Saturn Court, Sudbury, Ontario P3E 6B8' } }
       expect(response).to redirect_to root_path
 
       friend = Friend.last
       expect(friend.name).to eq('John')
       expect(friend.address).to eq('21 Saturn Court, Sudbury, Ontario P3E 6B8')
+      expect(friend.user).to eq(user)
+    end
+
+    it "should properly deal with validation errors" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      friend_count = Friend.count
+      post :create, params: { friend: { name: '', address: '' } }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(friend_count).to eq Friend.count
+    end
+
+  end
+
+  describe "friends#edit action" do
+    it "should successfully show the edit form if the friend is found" do
+
+    end
+
+    it "should return a 404 error message if the friend is not found" do
+
     end
   end
 
